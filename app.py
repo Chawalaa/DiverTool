fddf3b570bd54440687907cdce82a6920d69c19d
Chawala Banda <chawala.banda@keio.jp>
2026-05-20 17:12:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 # Page Config
 # -----------------------------
 st.set_page_config(
-    page_title="Teacher Communication Toolkit",
+    page_title="DOTS Toolkit",
     page_icon="🌱",
     layout="wide",
     initial_sidebar_state="expanded",
